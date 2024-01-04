82dd69b7a02f5be5f8f6ab267a8de47c8029d403
yassineesr <esryassine@gmail.com>
2024-01-04 18:10:14 +0100

    3 commit

--- POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello  :)";
+        return "Hello yassine  :)";
     }
 
     @GetMapping("/user")
